b6f6e93a2b627471f3a14a2ceae60f0f0457bac8
Michael Cousins <cousmj@gmail.com>
2013-09-26 15:44:50 -0500

    got some structure set up

---
+++ firmware/src/clockblock.cpp
@@ -0,0 +1,126 @@
+// clockblock
+// Copyright 2013 by Wiley Cousins, LLC.
+// shared under the terms of the MIT License
+//
+// file: clockblock.cpp
+// description: application file for the clockblock
+
+// defines
+#define TIME_OVF (3600*12)
+
+// AVR includes
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+// application includes
+#include "clockblock.h"
+//#include "Cereal.h"
+//#include "StuPId.h"
+
+// global variables
+// 12-hour time in seconds
+volatile int16_t time12 = 0;
+// time has been updated by the RTC
+volatile bool timeUpdate = false;
+// time has been updated by the user
+volatile bool timeSet = false;
+
+// global objects
+// USART interface at specified baudrate
+//Cereal debug;
+// SPI interface at specified frequency (MHz)
+//StuPId spi;
+// DS3234 Real-time clock module (spi channel, chip select on pin C2, reset on pin C3)
+//DS3234 rtc(&spi, PORTC, 2, PORTC, 3);
+// TLC5971 LED driver (3 drivers, serial clock on pin C5, serial data on pin C4)
+//TLC5971 leds(3, PORTC, 5, PORTC, 4);
+
+// main
+int main() {
+  // probably run some initializations
+  // initialize the USART for debugging at 9600 bps
+  //debug.init(9600);
+  // initialize the SPI to operate at 100 MHz
+  //spi.init(100);
+  // initialize the led driver at 100% current
+  //leds.init(255);
+
+/*
+  // initialize the clock
+  // check the TXCO stop bit to see if the RTC has a good time stored
+  if (!rtc.gotTime()) {
+    // make a brash assumtion about what time it is
+    time12 = 42;
+    timeSet = true;
+  }
+  // otherwise, get the time from the RTC
+  else {
+    time12 = rtc.getTime12();
+    timeUpdate = true;
+  }
+  // set the rtc to trigger an interrupt every second using alarm 1
+  rtc.setSecondTrigger(1);
+*/
+
+  // get lost
+  for (;;) {
+    if (setTime) {
+      rtc.setTime(time12);
+      setTime = false;
+      timeUpdate = true;
+    }
+    // if time has updated, update the LEDs
+    if (timeUpdate) {
+      // update the LEDs
+      updateArms();
+      // clear the flag
+      timeUpdate = false;
+    }
+  }
+
+  // one day we might get the answer
+  return 42;
+}
+
+// update the clock arms
+void updateArms() {
+
+
+}
+
+// --------------------------------------------- //
+// ISRs
+// --------------------------------------------- //
+
+// RTC interrupt on pin D2 - external interrupt 0
+ISR(INT0_vect) {
+  // increment and check for overflow
+  if(++time12 >= TIME_OVF) {
+    time12 = 0;
+  }
+}
+
+// hour set interrupt on pin D7 - pin change interrupt 23
+// minute set interrupt on pin D6 - pin change interrupt 22
+ISR(PCINT2_vect) {
+  // debounce the switch(es)
+  _delay_ms(20);
+
+  // check if hour pin is low
+  if ( !(PIND & (1<<7)) ) {
+    time += 3600;
+    timeSet = true;
+  }
+
+  // check if minute pin is low
+  if ( !(PIND & (1<<7)) ) {
+    time += 60;
+    timeSet = true;
+  }
+
+  // check for overflow
+  if (time12 >= TIME_OVF) {
+    time12 = time12 - TIME_OVF;
+  }
+}
