8bd3ba9b0ca6faaf082fd57def4082478839282c
Michael Cousins <cousmj@gmail.com>
2013-09-27 16:12:33 -0500

    starting library for DS3234 class

---
+++ firmware/lib/DS3234/DS3234.h
@@ -0,0 +1,6 @@
+// clockblock
+// Copyright 2013 by Wiley Cousins, LLC.
+// shared under the terms of the MIT License
+//
+// file: clockblock.cpp
+// description: header for DS3234 real-time clock class
