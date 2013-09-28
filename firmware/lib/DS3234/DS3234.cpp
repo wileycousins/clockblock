8ebdc91fb00bda4aa27b4015f0661d4ba817ab60
Michael Cousins <cousmj@gmail.com>
2013-09-28 13:45:30 -0500

    SPI helper functions set up

--- firmware/lib/DS3234/DS3234.cpp
+++ firmware/lib/DS3234/DS3234.cpp
@@ -4,3 +4,69 @@
 //
 // file: clockblock.cpp
 // description: class for DS3234 real-time clock
+
+#include "DS3234.h"
+#include "SPI.h"
+
+// can we build it? yes we can!
+// parameters:
+//   SPI channel (implement later when I've written an SPI library)
+//   chip select port, chip select pin
+//   reset port, reset pin
+//   interrupt port, interrupt pin
+DS3234::DS3234( /* StuPId *spi, */ uint8_t csPo, uint8_t csPi, uint8_t rstPo, uint8_t rstPi, uint8_t intPo, uint8_t intPi) {
+  // save this shit
+  // SPI channel
+  // deal with this later
+  // chip select pin
+  csPort = csPo;
+  csPin = csPi;
+  // reset
+  rstPort = rstPo;
+  rstPin = rstPi;
+  // interrupt
+  intPort = intPo;
+  intPin = intPi;
+}
+
+// SPI helpers
+// start / end
+// ensures SPI options are set correctly and pulls the chip select line low / high to start / end a transfer
+void DS3234::spiStart() {
+  // DS3234 is MSB first
+  SPI.setBitOrder(MSBFIRST);
+  // runs at up to 4MHz. dividing clock by 4 will ensure this isn't exceeded
+  SPI.setClockDivider(SPI_CLOCK_DIV4);
+  // supports SPI modes 1 and 3 (autodetects). let's use 1 (CPOL = 0, CPHA = 1)
+  SPI.setDataMode(SPI_MODE1);
+  // finally pull the chip select line low to begin the transfer
+  *csPort &= ~(1 << csPin);
+}
+
+void DS3234::spiEnd() {
+  // pull the chip select line high to end the transfer
+  *csPort |= (1 << csPin);
+}
+
+// read / write
+// parameters:
+//   register address to start at
+//   number of bytes to transfer
+//   buffer to read to / write from
+void DS3234::readReg(uint8_t reg, uint8_t n, uint8_t *data) {
+  spiStart();
+  SPI.transfer(reg);
+  for (uint8_t i=0; i<n; i++) {
+    data[i] = SPI.transfer(0x00);
+  }
+  spiEnd();
+}
+
+void DS3234::writeReg(uint8_t reg, uint8_t n, uint8_t *data) {
+  spiStart();
+  SPI.transfer(DS3234_SPI_WRITE | reg);
+  for (uint8_t i=0; i<n; i++) {
+    SPI.transfer(data[i]);
+  }
+  spiEnd();
+}
--- firmware/lib/DS3234/DS3234.h
+++ firmware/lib/DS3234/DS3234.h
@@ -8,6 +8,9 @@
 #ifndef DS3234_H
 #define DS3234_H
 
+// woo! typedefs!
+#include <stdint.h>
+
 // let's define some register addresses
 #define DS3234_SEC               0x00
 #define DS3234_MIN               0x01
@@ -32,27 +35,33 @@
 #define DS3234_SRAM_ADDR         0x18
 #define DS3234_SRAM_DATA         0x19
 
-
+// write command
+#define DS3234_SPI_WRITE         0x80
 
 class DS3234 {
-
 public:
-    // constructor
-    DS3234();
+  // constructor
+  DS3234( /* StuPId *spi, */ volatile uint8_t *csPo, uint8_t csPi, volatile uint8_t *rstPo, uint8_t rstPi, volatile uint8_t *intPo, uint8_t intPi);
+  // initialize (sets pins up and stuff)
+  void init();
 
 private:
+  // SPI helpers
+  void spiStart();
+  void spiEnd();
+  void readReg(uint8_t reg, uint8_t *data);
+  void writeReg(uint8_t reg, uint8_t data);
 
-    // pin assignments
-    // chip select
-    uint8_t csPort;
-    uint8_t csPin;
-    // interrupt pin
-    uint8_t intPort;
-    uint8_t intPin;
-    // reset pin
-    uint8_t rstPort;
-    uint8_t rstPin;
-
+  // pin assignments
+  // chip select
+  volatile uint8_t *csPort;
+  uint8_t csPin;
+  // reset pin
+  volatile uint8_t *rstPort;
+  uint8_t rstPin;
+  // interrupt pin
+  volatile uint8_t *intPort;
+  uint8_t intPin;
 
 };
 
