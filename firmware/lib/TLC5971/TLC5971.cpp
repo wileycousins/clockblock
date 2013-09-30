2698cb00cecc4aec279e835e095f79f68e17ecd3
Michael Cousins <cousmj@gmail.com>
2013-09-30 01:02:40 -0500

    ok so haven't really done any thinking yet but there's some stuff there

--- firmware/lib/TLC5971/TLC5971.cpp
+++ firmware/lib/TLC5971/TLC5971.cpp
@@ -5,3 +5,19 @@
 // file: TLC5971.cpp
 // description: TLC5971 LED-driver class
 
+#include "TLC5971.h"
+
+TLC5971::TLC5971(uint8_t n, volatile uint8_t *sckPo, uint8_t sckPi, volatile uint8_t *mosiPo, uint8_t mosiPi) {
+  // save pin defs
+  sckPort = sckPo;
+  sckPin = sckPi;
+  mosiPort = mosiPo;
+  mosiPin = mosiPi;
+
+  // should probably malloc an the data buffer for sending shit. or figure out a way to do that without mallocing anything ever
+}
+
+// initialize pins as outputs and hang out
+void TLC5971::init() {
+
+}
--- firmware/lib/TLC5971/TLC5971.h
+++ firmware/lib/TLC5971/TLC5971.h
@@ -4,3 +4,35 @@
 //
 // file: TLC5971.h
 // description: header for TLC5971 LED-driver
+
+#ifndef LIB_TLC5971_H
+#define LIB_TLC5971_H
+
+// typedefs (uint8_t's all day long baby)
+#include <stdint.h>
+
+class TLC5971 {
+public:
+  // constructor takes number of drivers daisychained on one serial line and the pindefs
+  TLC5971(uint8_t n, volatile uint8_t *sckPo, uint8_t sckPi, volatile uint8_t *mosiPo, uint8_t mosiPi);
+  // initialization routine
+  init();
+
+private:
+  // serial helpers
+  // pulse the serial clock
+  void pulseSck();
+  // set mosi and pulse the clock
+  void sendBit();
+
+  // number of drivers
+  uint8_t num;
+
+  // pin defs
+  volatile uint8_t *sckPort;
+  uint8_t sckPin;
+  volatile uint8_t *mosiPort;
+  uint8_t mosiPin;
+};
+
+#endif
