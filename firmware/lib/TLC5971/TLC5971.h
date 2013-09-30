d818543e6eb1a9856b823f76f9c2626b41456ca1
Michael Cousins <cousmj@gmail.com>
2013-09-30 13:42:52 -0500

    fleshing out the class structure

--- firmware/lib/TLC5971/TLC5971.h
+++ firmware/lib/TLC5971/TLC5971.h
@@ -11,12 +11,23 @@
 // typedefs (uint8_t's all day long baby)
 #include <stdint.h>
 
+// function control defines (function control is the 5 MSb's of the data package)
+#define TLC5971_BLANK   (1<<0)
+#define TLC5971_DSPRPT  (1<<1)
+#define TLC5971_TMGRST  (1<<2)
+#define TLC5971_EXTGCK  (1<<3)
+#define TLC5971_OUTTMG  (1<<4)
+
+
+
 class TLC5971 {
 public:
   // constructor takes number of drivers daisychained on one serial line and the pindefs
   TLC5971(uint8_t n, volatile uint8_t *sckPo, uint8_t sckPi, volatile uint8_t *mosiPo, uint8_t mosiPi);
   // initialization routine
-  init();
+  void init();
+  // control the chip
+  void setLeds(uint8_t *)
 
 private:
   // serial helpers
@@ -24,6 +35,24 @@ private:
   void pulseSck();
   // set mosi and pulse the clock
   void sendBit();
+  // send the write command to the device (6-bits = 0x25 = 0b100101)
+  void sendWriteCommand();
+  // send the function control packet
+  void sendFC();
+  // send the brightness control
+  void sendBC();
+  // send the greyscale
+  void sendGS();
+  // send a full 224 bit package
+  void sendDataPackage();
+
+  // buffer pointers
+  // function control
+  uint8_t *fc;
+  // brightness control
+  uint8_t *bc;
+  // greyscale control
+  uint8_t *gs;
 
   // number of drivers
   uint8_t num;
