104378dcf4eeb851f4d5cab12e8ec891e5e22ec0
Michael Cousins <cousmj@gmail.com>
2013-09-30 00:48:35 -0500

    adding files for TLC5971 led driver class

---
+++ firmware/lib/TLC5971/TLC5971.cpp
@@ -0,0 +1,7 @@
+// clockblock
+// Copyright 2013 by Wiley Cousins, LLC.
+// shared under the terms of the MIT License
+//
+// file: TLC5971.cpp
+// description: TLC5971 LED-driver class
+
---
+++ firmware/lib/TLC5971/TLC5971.h
@@ -0,0 +1,6 @@
+// clockblock
+// Copyright 2013 by Wiley Cousins, LLC.
+// shared under the terms of the MIT License
+//
+// file: TLC5971.h
+// description: header for TLC5971 LED-driver
