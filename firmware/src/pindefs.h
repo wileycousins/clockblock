7e4db6f61172c54bbb6e3f521211923e4c90ab8e
Michael Cousins <cousmj@gmail.com>
2013-10-22 14:12:03 -0400

    starting work on pindefs header

---
+++ firmware/src/pindefs.h
@@ -0,0 +1,12 @@
+// clockblock
+// Copyright 2013 by Wiley Cousins, LLC.
+// shared under the terms of the MIT License
+//
+// file: pindefs.h
+// description: port and pin definitions
+
+#ifndef CLOCKBLOCK_PINDEFS_H
+#define CLOCKBLOCK_PINDEFS_H
+
+
+#endif
