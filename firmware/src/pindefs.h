755988d07088a8c74817ca68571001bdd3b29a06
Michael Cousins <cousmj@gmail.com>
2013-10-22 17:22:40 -0400

    added in defines for v0.1 pcb

--- firmware/src/pindefs.h
+++ firmware/src/pindefs.h
@@ -15,6 +15,9 @@
 #define DDR(port) (*(&port-1))
 #define PIN(port) (*(&port-2))
 
+#ifdef BREADBOARD
+// **********************************
+// PIN DEFINES FOR BREADBOARD EDITION
 // SPI
 #define SPI_MOSI_DDR  DDRB
 #define SPI_MOSI_PIN  3
@@ -50,5 +53,49 @@
 #define INPUT_PCMSK  PCMSK1
 #define INPUT_PCICR  PCICR
 #define INPUT_PCIE   (1<<PCIE1)
+// END PIN DEFINES FOR BREADBOARD EDITION
+// **************************************
+
+#else
+// ********************
+// PIN DEFINES FOR V0.1
+// SPI
+#define SPI_MOSI_DDR  DDRB
+#define SPI_MOSI_PIN  3
+#define SPI_SCK_DDR   DDRB
+#define SPI_SCK_PIN   5
+
+// RTC
+// RTC pins
+#define RTC_CS_PORT   PORTC
+#define RTC_CS_PIN    2
+#define RTC_RST_PORT  PORTC
+#define RTC_RST_PIN   3
+#define RTC_INT_PORT  PORTD
+#define RTC_INT_PIN   2
+// avr external interrupt
+#define RTC_EXT_INT       0
+#define RTC_EXT_INT_vect  INT0_vect
+
+// TLC5971
+#define TLC_N          3
+#define TLC_MOSI_PORT  PORTC
+#define TLC_MOSI_PIN   4
+#define TLC_SCK_PORT   PORTC
+#define TLC_SCK_PIN    5
+
+// switches
+// switch pins
+#define INPUT_PORT  PORTD
+#define INPUT_HOUR  (1<<7)
+#define INPUT_MIN   (1<<6)
+//#define INPUT_MODE
+// switch interrupts
+#define INPUT_PCMSK  PCMSK2
+#define INPUT_PCICR  PCICR
+#define INPUT_PCIE   (1<<PCIE2)
+// END PIN DEFINES FOR V0.1
+// ************************
+#endif
 
 #endif
